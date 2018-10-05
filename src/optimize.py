8e2a1af297eae4984b4572412ab8b6dcc68108db
Ashish Trehan <ashishtrehan10@gmail.com>
2018-10-05 13:53:57 -0400

    optimize.py works

--- src/optimize.py
+++ src/optimize.py
@@ -1,36 +1,25 @@
-from util import build_matrix
-from ortools.constraint_solver import pywrapcp
-from ortools.constraint_solver import routing_enums_pb2
+from genetic_algorithm import *
+import matplotlib as plt
+import random
+
+cityList = []
+
+for i in range(0, 25):
+    cityList.append(City(x=int(random.random() * 200), y=int(random.random() * 200)))
+
+geneticAlgorithm(population=cityList, popSize=100, eliteSize=20, mutationRate=0.01, generations=500)
 
 
+def geneticAlgorithmPlot(population, popSize, eliteSize, mutationRate, generations):
+    pop = initialPopulation(popSize, population)
+    progress = []
+    progress.append(1 / rankRoutes(pop)[0][1])
 
-matrix = build_matrix(10,10)
-tsp_size = matrix.shape[0]
-city_names = [1,2,3,4,5,6,7,8,9,10]
-num_routes = 1
-depot = 0
+    for i in range(0, generations):
+        pop = nextGeneration(pop, eliteSize, mutationRate)
+        progress.append(1 / rankRoutes(pop)[0][1])
 
-#if tsp_size > 0:
-routing = pywrapcp.RoutingModel(tsp_size, num_routes, depot)
-search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
-# Create the distance callback.
-routing.SetArcCostEvaluatorOfAllVehicles(matrix)
-# Solve the problem.
-assignment = routing.SolveWithParameters(search_parameters)
-if assignment:
-    print ("Total distance: " + str(assignment.ObjectiveValue()) + " miles\n")
-
-    route_number = 0
-    index = routing.Start(route_number) # Index of the variable for the starting node.
-    route = ''
-    while not routing.IsEnd(index):
-        # Convert variable indices to node indices in the displayed route.
-        route += str(city_names[routing.IndexToNode(index)]) + ' -> '
-        index = assignment.Value(routing.NextVar(index))
-        route += str(city_names[routing.IndexToNode(index)])
-        print ("Route:\n\n" + route)
-    else:
-        print('No solution found.')
-
-
-
+    plt.plot(progress)
+    plt.ylabel('Distance')
+    plt.xlabel('Generation')
+    plt.show()
